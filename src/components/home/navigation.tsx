b5cee0b3ecef67971b30caf00ac9675fc40b3928
mozammal01 <mozammalhaq01@gmail.com>
2025-10-24 15:40:03 +0600

    Refactor Navigation component to replace static icons with Lucide icons for improved scalability and maintainability. Update styles for dark mode support and enhance link accessibility with proper alt text. Adjust logo handling for better responsiveness.

--- src/components/home/navigation.tsx
+++ src/components/home/navigation.tsx
@@ -1,53 +1,53 @@
 "use client";
 import logoWhite from "@/../public/logo/logo-white.png";
-import logoBlack from "@/../public/logo/logo-black.png";
-import ProjIcon from "@/../public/icons/navIcon/projects.png";
-import HomeIcon from "@/../public/icons/navIcon/home.png";
-import AboutIcon from "@/../public/icons/navIcon/about.png";
-import ContIcon from "@/../public/icons/navIcon/contact.png";
-import ServIcon from "@/../public/icons/navIcon/services.png";
-import BlogsIcon from "@/../public/icons/navIcon/blogs.png";
+import logoBlack from "@/../public/logo/logo-black.png";  
 import Image from "next/image";
 import Link from "next/link";
 
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { AnimatedThemeToggler } from "../ui/animated-theme-toggler";
+import { House, Mail, User } from "lucide-react";
+import { Brain } from "lucide-react";
+
+import { Folder } from "lucide-react";
+
+import { Server } from "lucide-react";
 
 export default function Navigation() {
   const icons = [
     {
       id: 1,
-      icon: HomeIcon,
+      icon: <House size={25} />,
       href: "#home",
       placeholder: "Home",
     },
     {
       id: 2,
-      icon: AboutIcon,
+      icon: <User size={25} />,
       href: "#about",
       placeholder: "About",
     },
     {
       id: 3,
-      icon: BlogsIcon,
+      icon: <Brain size={25} />,
       href: "#skills",
       placeholder: "Skills",
     },
     {
       id: 4,
-      icon: ProjIcon,
+      icon: <Folder size={25} />,
       href: "#projects",
       placeholder: "Projects",
     },
     {
       id: 5,
-      icon: ServIcon,
+      icon: <Server size={25} />,
       href: "#services",
       placeholder: "Services",
     },
     {
       id: 6,
-      icon: ContIcon,
+      icon: <Mail size={25} />,
       href: "#contact",
       placeholder: "Contact",
     },
@@ -58,23 +58,23 @@ export default function Navigation() {
       <div
         className={`md:flex hidden flex-col gap-4 justify-between items-center h-screen px-4 top-0 right-0 py-10 fixed bg-black/15 dark:bg-white/50 z-50`}
       >
-        <div>
+        <Link href="#home">
           <div className="dark:hidden">
             <Image src={logoWhite} alt="Home" width={60} height={60} />
           </div>
           <div className="hidden dark:block">
             <Image src={logoBlack} alt="Home" width={60} height={60} />
           </div>
-        </div>
+        </Link>
         <div className="flex flex-col gap-8">
           {icons.map((icon, index) => (
             <Tooltip key={index}>
               <TooltipTrigger asChild>
                 <Link
                   href={icon.href}
-                  className="hover:bg-[#ffc107] hover:text-white transition-all scale-100 hover:scale-120 duration-300 rounded-full p-4"
+                  className="hover:bg-[#ffc107] hover:text-white transition-all scale-100 hover:scale-120 duration-300 rounded-full p-4 dark:text-black text-white font-extrabold"
                 >
-                  <Image src={icon.icon} alt={icon.placeholder} width={20} height={20} />
+                  {icon.icon}
                 </Link>
               </TooltipTrigger>
               <TooltipContent>
@@ -84,7 +84,7 @@ export default function Navigation() {
           ))}
         </div>
         <div className="hover:bg-primary size-10 inline-flex items-center justify-center transition-all scale-100 hover:scale-110 duration-300 rounded-full">
-          <AnimatedThemeToggler className={`text-black`} />
+          <AnimatedThemeToggler className={`text-white dark:text-black`} />
         </div>
       </div>
     </div>
