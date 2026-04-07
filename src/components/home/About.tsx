994e0b5c7fdc1602c4d77b1283ce937b9520d17a
Mozammal Haque <mozammalhaq01@gmail.com>
2026-04-07 18:13:39 +0600

    Update resume in about section

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -35,7 +35,7 @@ export default function About() {
           <AnimatedButton
             variant="leftToRight"
             text="My Resume"
-            href="https://drive.google.com/file/d/1JXgJwpPaL8zN6Wb6G-DYOyAfIBF24jPS/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1N-9i3sdRz2sqKUOVK3rnvSCBNPxhJcDa/view?usp=drive_link"
             target="_blank"
           />
         </div>
